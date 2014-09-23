359d24efa67b55b1a2d74552713e4eb25f74722d
Robin Soetens <robinsoetens@gmail.com>
2014-09-23 14:14:44 +0000

    rearranging and added profiling tool
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/stem_tracker@27537 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ include/stem_tracker.h
@@ -0,0 +1,12 @@
+#ifndef STEM_TRACKER_H
+#define STEM_TRACKER_H
+
+/* for code profiling */
+StatsPublisher sp;
+
+/* for torso ref */
+int count = 20;
+int up = 1;
+double ref;
+
+#endif // STEM_TRACKER_H
--- src/stem_tracker.cpp
+++ src/stem_tracker.cpp
@@ -1,28 +1,43 @@
+/* ros includes */
 #include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <urdf/model.h>
+
+/* kdl for ros includes */
+#include <kdl_parser/kdl_parser.hpp>
+#include <kdl/tree.hpp>
+
+/* amigo tooling includes */
+#include <profiling/StatsPublisher.h>
+
+/* header for this node */
+#include "stem_tracker.h"
 
 int main(int argc, char** argv){
 
+    /* initialize node */
     ros::init(argc, argv, "stem_tracker");
     ros::NodeHandle n;
-
     ros::Publisher joint_pub = n.advertise<sensor_msgs::JointState>("/amigo/torso/references", 1000);
     ros::Rate loop_rate(10);
 
-    int count = 20;
-    int up = 1;
-    double ref;
+    /* initialize profiling */
+    sp.initialize();
 
+    /* update loop */
     while(ros::ok()){
 
-        ROS_INFO("spinnin, ref = %f",ref);
+        /* start sample timing, for profiling */
+        sp.startTimer("main");
 
-        if(count > 95 || count < 20)
+        /* calculated desired joint reference */
+        if(count > 90 || count < 20)
             up = -up;
 
         count=count+up;
-        ref = ( (double)count ) /200.0;
+        ref = ( (double)count ) / 200.0;
 
+        /* publish updated joint reference */
         sensor_msgs::JointState msg;
 
         msg.name.push_back("torso_joint");
@@ -30,7 +45,13 @@ int main(int argc, char** argv){
         msg.header.stamp = ros::Time::now();
 
         joint_pub.publish(msg);
+        ROS_INFO("spinnin, ref = %f",ref);
 
+        /* stop and publish sample time, for profiling */
+        sp.stopTimer("main");
+        sp.publish();
+
+        /* wait until next sample */
         ros::spinOnce();
         loop_rate.sleep();
 
