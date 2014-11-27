719851c0e7edc5dd1db58100dd288a68d3ba1675
Robin Soetens <robinsoetens@gmail.com>
2014-11-27 15:01:56 +0000

    cleanup of whisker interpreter
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/stem_tracker@28328 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- include/whiskerinterpreter.h
+++ include/whiskerinterpreter.h
@@ -2,42 +2,48 @@
 #define WHISKERINTERPRETER_H
 
 #include <vector>
-#include <cmath>
-#include <ros/ros.h>
-#include <visualization_msgs/Marker.h>
-#include <geometry_msgs/Point.h>
+#include <string>
 
-
-#define INFO_STREAM     ROS_INFO_STREAM
+/* Status:
+ * 0 - unknown
+ * 1 - gripper not around stem
+ * 2 - gripper around stem but touching whiskers
+ * 3 - gripper around stem and not touching whiskers */
 
 class WhiskerInterpreter
 {
-    private:
-        int m_n_whiskers;
-        int m_gripper_id;
-        float m_whisker_length;
-        float m_gripper_diameter;
-        float m_gripper_radius;
-        float m_max_whisker_force;
-        int m_status; // 0 - unknown, 1 - gripper not around stem, 2 - gripper around stem but touching whiskers, 3 - gripper around stem and not touching whiskers
-        std::vector<float> m_whisker_force;
-        std::vector<float> m_estimated_pos_error;
 
-    public:
-        WhiskerInterpreter(int gripper_id);
-        bool selfCheck();
-        int getGripperID();
-        void setGripperDiameter(float gripper_diameter);
-        void setWhiskerLength(float whisker_length);
-        void setMaxWhiskerForce(float max_whisker_force);
-        void setNumberOfWhiskers(int n_whiskers);
-        int getStatus();
-        void simulateWhiskerGripper(std::vector<float> gripper_center, std::vector<float> stem_center);
-        std::vector<float> getWhiskerNetForce();
-        void showForceInRviz(ros::Publisher* p_vis_pub, std::vector<float> gripper_xyz);
-        std::vector<float> getXYerror();
+private:
 
-        ~WhiskerInterpreter();
+    int m_n_whiskers;
+    int m_gripper_id;
+    float m_whisker_length;
+    float m_gripper_diameter;
+    float m_gripper_radius;
+    float m_max_whisker_force;
+    int m_status;
+    std::vector<float> m_whisker_force;
+    std::vector<float> m_estimated_pos_error;
+
+public:
+
+    WhiskerInterpreter(int gripper_id) : m_gripper_id(gripper_id), m_status(0) {}
+
+    bool selfCheck() const;
+
+    void setGripperDiameter(const float& gripper_diameter);
+    void setWhiskerLength(const float& whisker_length);
+    void setMaxWhiskerForce(const float& max_whisker_force);
+    void setNumberOfWhiskers(const int& n_whiskers);
+
+    const int& getGripperID() const { return m_gripper_id; }
+    const int& getStatus() const { return m_status; }
+    const std::vector<float>& getWhiskerNetForce() const { return m_whisker_force; }
+    const std::vector<float>& getXYerror() const { return m_estimated_pos_error; }
+
+    void simulateWhiskerGripper(const std::vector<float>& gripper_center, const std::vector<float>& stem_center);
+
+    ~WhiskerInterpreter();
 };
 
 #endif // WHISKERINTERPRETER_H
--- src/whiskerinterpreter.cpp
+++ src/whiskerinterpreter.cpp
@@ -1,82 +1,64 @@
 #include "whiskerinterpreter.h"
+#include <cmath>
+#include "ros/ros.h"  /* Only for ROS_ROS_INFO_STREAM */
 
-WhiskerInterpreter::WhiskerInterpreter(int gripper_id)
-{
-    m_gripper_id = gripper_id;
-    m_status = 0;
-}
 
-int WhiskerInterpreter::getGripperID()
-{
-    return m_gripper_id;
-}
-
-void WhiskerInterpreter::setNumberOfWhiskers(int n_whiskers)
+void WhiskerInterpreter::setNumberOfWhiskers(const int& n_whiskers)
 {
     m_n_whiskers = n_whiskers;
 }
 
-void WhiskerInterpreter::setGripperDiameter(float gripper_diameter)
+void WhiskerInterpreter::setGripperDiameter(const float& gripper_diameter)
 {
     m_gripper_diameter = gripper_diameter;
     m_gripper_radius = gripper_diameter / 2.0f;
 }
 
-void WhiskerInterpreter::setWhiskerLength(float whisker_length)
+void WhiskerInterpreter::setWhiskerLength(const float& whisker_length)
 {
     m_whisker_length = whisker_length;
 }
 
-void WhiskerInterpreter::setMaxWhiskerForce(float max_whisker_force)
+void WhiskerInterpreter::setMaxWhiskerForce(const float& max_whisker_force)
 {
     m_max_whisker_force = max_whisker_force;
 }
 
-int WhiskerInterpreter::getStatus()
-{
-    return m_status;
-}
-
-bool WhiskerInterpreter::selfCheck()
+bool WhiskerInterpreter::selfCheck() const
 {
 
     bool IamOK = true;
 
     if (m_n_whiskers <= 0)
     {
-        INFO_STREAM("In whisker gripper with id " << m_gripper_id << " number of whiskers set to zero or negative number");
+        ROS_INFO_STREAM("In whisker gripper with id " << m_gripper_id << " number of whiskers set to zero or negative number");
         IamOK = false;
     }
 
     if (m_whisker_length > m_gripper_radius)
     {
-        INFO_STREAM("in whisker gripper with id " << m_gripper_id << " length of whiskers is larger than radius of gripper");
+        ROS_INFO_STREAM("in whisker gripper with id " << m_gripper_id << " length of whiskers is larger than radius of gripper");
         IamOK = false;
     }
 
     return IamOK;
 }
 
-void WhiskerInterpreter::simulateWhiskerGripper(std::vector<float> gripper_center, std::vector<float> stem_center)
+void WhiskerInterpreter::simulateWhiskerGripper(const std::vector<float>& gripper_center, const std::vector<float>& stem_center)
 {
-
-    /* takes two doubles (xy) as coordinates of the gripper two doubles as coordinate of
-             * the stem (both are in the same z-plane).
-             * returns a force (xy) with origin at gripper center */
-
     m_whisker_force.clear();
     m_estimated_pos_error.clear();
 
     if(gripper_center.size() < 2 )
     {
-        INFO_STREAM("in simulateWhiskerGripper gripper center xy needed!");
+        ROS_INFO_STREAM("in simulateWhiskerGripper gripper center xy needed!");
         m_status = 0;
         return;
     }
 
     if(stem_center.size() < 2 )
     {
-        INFO_STREAM("in simulateWhiskerGripper stem center xy needed!");
+        ROS_INFO_STREAM("in simulateWhiskerGripper stem center xy needed!");
         m_status = 0;
         return;
     }
@@ -106,68 +88,6 @@ void WhiskerInterpreter::simulateWhiskerGripper(std::vector<float> gripper_cente
         m_whisker_force.at(0) = (m_estimated_pos_error[0] / dist_gripper_to_stem) * m_max_whisker_force * whisker_fraction_deformed;
         m_whisker_force.at(1) = (m_estimated_pos_error[1] / dist_gripper_to_stem) * m_max_whisker_force * whisker_fraction_deformed;
     }
-
-}
-
-std::vector<float> WhiskerInterpreter::getXYerror()
-{
-    // to do: turn measured force/deflection into estimation of position error
-    return m_estimated_pos_error;
-}
-
-std::vector<float> WhiskerInterpreter::getWhiskerNetForce()
-{
-    return m_whisker_force;
-}
-
-void WhiskerInterpreter::showForceInRviz(ros::Publisher* p_vis_pub, std::vector<float> gripper_xyz)
-{
-
-    if(!selfCheck()){
-        return;
-    }
-
-    if(gripper_xyz.size() != 3){
-        INFO_STREAM("need gripper location xyz to visualize whisker force!");
-        return;
-    }
-
-    if(m_whisker_force.size() != 2){
-        INFO_STREAM("trying to show a force vector that does not exist!");
-        return;
-    }
-
-    /* construct line strip marker object */
-    visualization_msgs::Marker marker;
-    marker.header.frame_id = "/amigo/base_link";
-    marker.header.stamp = ros::Time().now();
-    marker.id = 3;
-    marker.type = visualization_msgs::Marker::ARROW;
-    marker.action = visualization_msgs::Marker::ADD;
-    marker.pose.orientation.w = 1.0;
-    marker.ns = "whisker_force";
-    marker.scale.x = 0.01;
-    marker.scale.y = 0.02;
-    marker.color.a = 1.0f;
-    marker.color.r = 0.0f;
-    marker.color.g = 0.0f;
-    marker.color.b = 1.0f;
-
-    /* construct nodes point */
-    geometry_msgs::Point p_start, p_end;
-
-    p_start.x = gripper_xyz.at(0) - m_whisker_force.at(0);
-    p_start.y = gripper_xyz.at(1) - m_whisker_force.at(1);
-    p_start.z = gripper_xyz.at(2);
-    marker.points.push_back(p_start);
-
-    p_end.x = gripper_xyz.at(0);
-    p_end.y = gripper_xyz.at(1);
-    p_end.z = gripper_xyz.at(2);
-    marker.points.push_back(p_end);
-
-    /* publish marker */
-    p_vis_pub->publish( marker );
 
 }
 
