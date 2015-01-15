a2be547d0d91efa3a67f6c4f01e1d00990a81647
robin <robinsoetens@gmail.com>
2015-01-15 17:23:18 +0100

    startup live rostopic plots for multiple whiskers

---
+++ tools/show_live_plots.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+rqt_plot /amigo/whiskergripper/measurements13:measurements14:measurements15:measurements16:measurements17:measurements18 > /dev/null &
+rqt_plot /amigo/whiskergripper/measurements7:measurements8:measurements9:measurements10:measurements11:measurements12 > /dev/null &
