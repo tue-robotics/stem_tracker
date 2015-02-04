bc501e08b9241f1c94f27343ea1eb71546f88111
Ton Peters <peterstmc@gmail.com>
2015-02-04 17:06:58 +0100

    adjust plotting tool for different whisker value msg type

--- tools/show_live_plots.sh
+++ tools/show_live_plots.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 
-rqt_plot /amigo/whiskergripper/measurements15:measurements16:measurements17:measurements18 > /dev/null &
-rqt_plot /amigo/whiskergripper/measurements13:measurements14:measurements11:measurements12 > /dev/null &
+rqt_plot /amigo/whiskergripper/measurements/data[0]:data[1]:data[2]:data[3] > /dev/null &
+rqt_plot /amigo/whiskergripper/measurements/data[4]:data[5]:data[6]:data[7]:data[8] > /dev/null &
+
+#rqt_plot /amigo/whiskergripper/measurements/data[0] > /dev/null &
