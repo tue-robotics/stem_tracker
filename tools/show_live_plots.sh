e22926deb720824f7c411b6abd3226982534b9c4
Robin Soetens <robinsoetens@gmail.com>
2015-02-26 13:07:04 +0100

    reconnected wires

--- tools/show_live_plots.sh
+++ tools/show_live_plots.sh
@@ -1,8 +1,8 @@
 #/bin/bash
 
-rqt_plot /amigo/whiskergripper/whisker_measurements/data[9]:data[10]:data[11] > /dev/null &
-rqt_plot /amigo/whiskergripper/whisker_measurements/data[6]:data[7]:data[8] > /dev/null &
-rqt_plot /amigo/whiskergripper/whisker_measurements/data[3]:data[4]:data[5] > /dev/null &
-rqt_plot /amigo/whiskergripper/whisker_measurements/data[0]:data[1]:data[2] > /dev/null &
+rqt_plot /amigo/whiskergripper/whisker_measurements/data[8]:data[9]:data[10]:data[11] > /dev/null &
+rqt_plot /amigo/whiskergripper/whisker_measurements/data[4]:data[5]:data[6]:data[7] > /dev/null &
+rqt_plot /amigo/whiskergripper/whisker_measurements/data[0]:data[1]:data[2]:data[3] > /dev/null &
 
 rqt_plot /amigo/whiskergripper/top_measurements/data[0]:data[1]:data[2]:data[3]:data[4] > /dev/null &
+
